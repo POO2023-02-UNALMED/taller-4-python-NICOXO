a77a2b56e3d327c6b0b5be19b0ede4af9e12826b
NICOXO <joduquel@unal.edu.co>
2023-09-05 10:36:28 -0500

    Taller4

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -8,9 +8,9 @@ class Grupo:
         self._asignaturas = asignaturas if asignaturas is not None else []
         self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
-    def listadoAsignaturas(self, *args):
-        for asignatura in args:
-            self._asignaturas.append(Asignatura(asignatura, ""))
+    def listadoAsignaturas(self, asignaturas):
+        for asignatura in asignaturas:
+            self._asignaturas.append(Asignatura(asignatura))
 
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
@@ -21,7 +21,3 @@ class Grupo:
     @classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
-
-    @staticmethod
-    def asignarNombreEstático(nombre="Grado 6"):
-        Grupo.grado = nombre
